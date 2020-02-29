3f93b319fde9845f2cc0a55e7cdd948f80c181f6
pashaeryshken <pashaeryshken64@gmail.com>
2020-02-29 13:57:59 +0300

    add selenoid-docker

--- src/test/java/Fixtures/BrowserFixture.java
+++ src/test/java/Fixtures/BrowserFixture.java
@@ -5,16 +5,22 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
-import static com.codeborne.selenide.Selenide.open;
-
 @ExtendWith(ScreenShooterLog.class)
 public class BrowserFixture {
 
     @BeforeAll
     public static void startBrowser(){
         Configuration.baseUrl= "http://automationpractice.com/index.php";
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1980x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach

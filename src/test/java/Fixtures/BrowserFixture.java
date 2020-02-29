f85a609b5d31a0a645b738cf7bcfaa6067460b29
pashaeryshken <pashaeryshken64@gmail.com>
2020-02-29 20:46:44 +0300

    no message

--- src/test/java/Fixtures/BrowserFixture.java
+++ src/test/java/Fixtures/BrowserFixture.java
@@ -2,28 +2,40 @@ package Fixtures;
 
 import Watchers.ScreenShooterLog;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import static com.codeborne.selenide.Selenide.clearBrowserCookies;
-@ExtendWith(ScreenShooterLog.class)
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import static com.codeborne.selenide.Selenide.*;
+
+/*@ExtendWith(ScreenShooterLog.class)*/
 public class BrowserFixture {
 
     @BeforeAll
-    public static void startBrowser(){
+    public static void startBrowser() throws MalformedURLException {
         Configuration.baseUrl= "http://automationpractice.com/index.php";
+        Configuration.browser= "chrome";
         Configuration.remote = "http://localhost:4444/wd/hub";
-        Configuration.browserSize = "1366x780";
+        Configuration.browserSize = "1980x1080";
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+
     }
 
     @AfterEach
     public void postConditions(){
         clearBrowserCookies();
     }
+
 }
